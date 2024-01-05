dbb1214b105f9b422a1cc2fca4b6df73d84df282
amiroslaw <arek47@gmail.com>
2024-01-05 17:48:59 +0100

    init

---
+++ src/main/java/ovh/miroslaw/kindle2anki/Kindle2ankiApplication.java
@@ -0,0 +1,13 @@
+package ovh.miroslaw.kindle2anki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kindle2ankiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kindle2ankiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ovh/miroslaw/kindle2anki/Kindle2ankiApplicationTests.java
@@ -0,0 +1,9 @@
+package ovh.miroslaw.kindle2anki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kindle2ankiApplicationTests {
+
+}
