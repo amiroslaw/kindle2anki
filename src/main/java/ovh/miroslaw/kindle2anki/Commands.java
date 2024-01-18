97ce6c139aa45cc9edc4ff62300dcc9acd849ef5
amiroslaw <arek47@gmail.com>
2024-01-18 22:45:21 +0100

    via pojo

--- src/main/java/ovh/miroslaw/kindle2anki/Commands.java
+++ src/main/java/ovh/miroslaw/kindle2anki/Commands.java
@@ -1,7 +1,14 @@
 package ovh.miroslaw.kindle2anki;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.shell.command.annotation.Command;
+import ovh.miroslaw.kindle2anki.model.Root;
+
+import java.util.List;
 
 @Command
 @RequiredArgsConstructor
@@ -11,6 +18,19 @@ public class Commands {
 
     @Command(alias = "t")
     public String test() {
-        return mwClient.getDefinition("advance");
+        String json = mwClient.getDefinition("matrix");
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<List<Root>> typeReference = new TypeReference<>() {};
+        try {
+//            List<Root> root = mapper.readValue(json, typeReference);
+            List<Root> root = mapper.readValue(json, typeReference);
+            System.out.println(root);
+            JsonNode defnitions = mapper.readValue(json, JsonNode.class);
+
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return json;
     }
 }
